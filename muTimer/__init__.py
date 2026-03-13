2f79723c266b014dd2d3b0442ad23228a7837bb3
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2026-03-13 13:23:13 +0100

    BUILD: Dynamic versioning

--- muTimer/__init__.py
+++ muTimer/__init__.py
@@ -1,5 +1,14 @@
+"""
+Hierarchical timing utility with nested context manager support.
+"""
+
 # MIT License. See LICENSE file for details.
 
 from .Timer import Timer
 
-__all__ = ["Timer"]
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "unknown"
+
+__all__ = ["Timer", "__version__"]
